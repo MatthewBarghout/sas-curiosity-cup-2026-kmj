0fb80e979a04e215ded269f5cf91b5763899c946
Juan Perdomo <jcperdomo100@gmail.com>
2026-02-04 13:41:59 -0500

    sentiment and other stuff

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -1,3 +1,31 @@
 """
 Data module
+
+Provides data fetching and sentiment analysis utilities.
 """
+
+from .fetch import (
+    download_stock_data,
+    calculate_returns,
+    calculate_mu_sigma,
+    download_vix,
+    get_vix_level,
+    get_vix_regime,
+    compare_vix_to_garch,
+    download_multiple_stocks,
+    calculate_returns_multiple
+)
+
+from .sentiment import (
+    GoogleTrendsSentiment,
+    download_google_trends,
+    get_sentiment_score,
+    download_ai_nvda_sentiment,
+    get_bubble_sentiment_indicator
+)
+
+from .multi_stock_fetch import (
+    MultiStockDataPipeline,
+    fetch_multiple_stocks,
+    get_default_stock_universe
+)
